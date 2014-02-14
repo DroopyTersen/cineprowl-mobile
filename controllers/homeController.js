a22fc0a09b932b67c7bc2cb11c308c7383da70eb
unknown <apetersen@LT0191.skyline.gb>
2014-02-14 13:13:32 -0600

    added owl sliders to homepage

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,10 +1,16 @@
+"use strict";
 var movieService = new(require("../../Services/MovieService"))();
-
+var Q = require("q");
 module.exports = {
 	index: function(req, res) {
-		movieService.queue().then(function(movies){
+		var recentPromise = movieService.query({}, {addedToDb: -1}, 0, 6);
+		var queuePromise = movieService.queue();
+		var favoritePromise = movieService.favorites();
+		Q.spread([recentPromise, queuePromise, favoritePromise], function(recent, queue, favorites) {
 			var viewModel = {
-				movies: movies,
+				recent: recent,
+				queue: queue,
+				favorites: favorites
 			};
 			res.render("home/index", viewModel);
 		});
