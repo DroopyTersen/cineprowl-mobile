7b8af665b46751d5ce7a7e509ddc6fc067047e37
unknown <apetersen@LT0191.skyline.gb>
2014-02-15 21:35:52 -0600

    added api and prolific actors

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,16 +1,22 @@
 "use strict";
 var movieService = new(require("../../Services/MovieService"))();
+var imageHelper = require("../../Models/imageHelper");
 var Q = require("q");
 module.exports = {
 	index: function(req, res) {
 		var recentPromise = movieService.query({}, {addedToDb: -1}, 0, 6);
 		var queuePromise = movieService.queue();
-		var favoritePromise = movieService.favorites();
-		Q.spread([recentPromise, queuePromise, favoritePromise], function(recent, queue, favorites) {
+		var actorsPromise = movieService.actors(16);
+		Q.spread([recentPromise, queuePromise, actorsPromise], function(recent, queue, actors) {
+			actors.forEach(function(actor){
+				actor.name = actor._id.name;
+				actor.id = actor._id.id;
+				actor.profileImage = imageHelper.profile.getMid(actor._id.profile_path);
+			});
 			var viewModel = {
 				recent: recent,
 				queue: queue,
-				favorites: favorites
+				actors: actors
 			};
 			res.render("home/index", viewModel);
 		});
