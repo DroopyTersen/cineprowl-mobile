91dda96447b0828cb78a07f2b3a0fe9cd79abb21
Andrew Petersen <Andrew Petersen>
2016-10-31 00:38:26 -0500

    removed auth

--- router.js
+++ router.js
@@ -4,19 +4,19 @@ module.exports = {
 		"use strict";
 		var controllers = require("./controllers");
 		var authentication = require("./authentication");
-		authentication.configureRoutes(app, passport);
+		//authentication.configureRoutes(app, passport);
 
-		app.get("/", authentication.check, controllers.home.index);
+		app.get("/", controllers.home.index);
 
-		app.get("/:controller/:action/:id", authentication.check, function(req, res) {
+		app.get("/:controller/:action/:id",  function(req, res) {
 			controllers[req.params.controller.toLowerCase()][req.params.action.toLowerCase()](req, res);
 		});
 
-		app.get("/:controller/:action", authentication.check, function(req, res) {
+		app.get("/:controller/:action", function(req, res) {
 			controllers[req.params.controller.toLowerCase()][req.params.action.toLowerCase()](req, res);
 		});
 
-		app.get("/:controller", authentication.check, function(req, res) {
+		app.get("/:controller", function(req, res) {
 			controllers[req.params.controller.toLowerCase()].index(req, res);
 		});
 	}
